0a690b59253222e66404b681f21a821c10bd0f21
Thomas Sayen <69324626+Chi-Iroh@users.noreply.github.com>
2026-03-27 18:19:56 +0100

    BREAKING CHANGE: test

--- server/__tests__/distance.ts
+++ server/__tests__/distance.ts
@@ -5,4 +5,4 @@ describe("distance", () => {
     test("(50;50) -> (-10;-10) --> should ~ 8 831,87 km", () => {
         expect(getDistance({ lng: 50, lat: 50 }, { lng: -10, lat: -10 })).toBeCloseTo(8831.87)
     })
-})
+})   
